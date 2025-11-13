2dad5d5b64a567047118c5591be638308c2efff3
iamhaziqazhar <haziqazhar1m@gmail.com>
2025-11-13 17:49:23 +0500

    video of states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.js';
+import TextForm from './components/TextForm.js';
 
 function App() {
   return (
     <>
 <Navbar title="textutils" about="About"/>
+<div className="container my-5"><TextForm heading="Please Enter your text here to Analyze:" /></div>
 
     </>
   );
